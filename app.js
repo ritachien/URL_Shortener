178245c9094344a964b8c9614696c0d6c371390f
ritachien <ritachien929@gmail.com>
2022-04-20 00:40:48 +0800

    feat: create body of handlebars(index.handlebars)

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
+// Import modules and set related variables
 const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
 
+// Set view engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+
+// Set Routes
 app.get('/', (req, res) => {
-  res.send('This is a URL shortener app.')
+  res.render('index')
 })
 
+app.post('/', (req, res) => {
+  const newUrl = req.body.url
+  console.log(newUrl)
+  res.render('index', { newUrl })
+})
+
+// Listen the server
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
 })
