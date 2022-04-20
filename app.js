173f0ae2e9bc06407eefc1d6205dae18c1f4b57c
ritachien <ritachien929@gmail.com>
2022-04-21 00:09:41 +0800

    feat: set route for shortenURL

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const UrlIndex = require('./models/urlIndex')
 const urlProduce = require('./utilities/urlProduce')
 
 const app = express()
+
+// User optional settings for shortenUrl
+// Default output: 'http://localhost:{port}/{5RandomText}'
 const port = 3000
 const urlBasic = `http://localhost:${port}/`
-
+const randomLength = 5
 
 // Set view engine
 app.engine('handlebars', exphbs.engine())
@@ -19,8 +22,6 @@ app.set('view engine', 'handlebars')
 // Middleware
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
-
-
 
 // Set Routes
 // Read: input URL to be converted
@@ -33,7 +34,7 @@ app.post('/', (req, res) => {
   // check if user input has already in index
   UrlIndex.findOne({ originalUrl: req.body.url })
     .then(urlData => {
-      const newUrl = urlProduce(5)
+      const newUrl = urlProduce(randomLength)
       return urlData ? urlData : UrlIndex.create({
         shortenUrl: newUrl,
         originalUrl: req.body.url
@@ -42,6 +43,20 @@ app.post('/', (req, res) => {
     .then(urlData => {
       urlData = urlData.toObject()
       res.render('index', { urlBasic, urlData })
+    })
+    .catch(err => console.log(err))
+})
+
+// Read: convert link from shortenURL to OriginalURL
+app.get('/:shortenUrl', (req, res) => {
+  const { shortenUrl } = req.params
+  UrlIndex.findOne({ shortenUrl })
+    .then(data => {
+      if (!data) {
+        const errorURL = `${urlBasic + shortenUrl}`
+        return res.render('index', { errorURL })
+      }
+      res.redirect(data.originalUrl)
     })
     .catch(err => console.log(err))
 })
