81737cfbb290cb5ad567994d962267c8800422f6
ritachien <ritachien929@gmail.com>
2022-04-20 18:29:10 +0800

    feat: set MongoDB connection with mongoose
    npm install:
    * mongoose @6.3.0
    * dotenv @16.0.0

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+require('dotenv').config({ path: 'config/.env' })
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('MongoDB error!')
+})
+
+db.once('open', () => {
+  console.log('MongoDB connected!')
+})
